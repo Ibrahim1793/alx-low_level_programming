584f97bb928a7dfc5bc14f8308fa6185bc8b476b
Ibrahim sultan <Ibrahimsultan6@gmail.com>
2021-06-18 18:48:43 +0000

    work on quastion 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/** main fumction 
+** for loop
+return 0 **/
+int main () {
+   char ch;
+
+   for(ch = 'a' ; ch <= 'z' ; ch++) {
+      putchar(ch);
+   }
+   
+   return(0);
+}
