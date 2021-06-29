364e4f2b595db706dde84bc7d91757f298a3e067
Ibrahim sultan <Ibrahimsultan6@gmail.com>
2021-06-29 05:43:46 +0000

    first commit of pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * function that reset the value to 98
+ * from out side function
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *  A function that swaps the values of two integers
+ * from outside function
+ */
+
+void swap_int(int *a, int *b);
+{
+	*a = b;
+	*b = a;
+}
