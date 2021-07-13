e3b4980f706cc12892d05ac95543c562fe079154
Ibrahim sultan <Ibrahimsultan6@gmail.com>
2021-07-13 19:11:35 +0000

    Edit solution for #2 and Add #3&4

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,14 +3,14 @@
 /**
  * *str_concat - concatenates two strings
  * @s1: first string
- @s2: second string
+ * @s2: second string
  * Return: pointer to new space in memory or null
  **/
 char *str_concat(char *s1, char *s2)
 {
 	char *strDup;
 	int i, j;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
