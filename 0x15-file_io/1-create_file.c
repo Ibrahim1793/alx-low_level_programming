f2f32ae1d0ded018ec032e28c2ae7c97f62fe754
Ibrahim sultan <Ibrahimsultan6@gmail.com>
2021-08-09 19:52:31 +0000

    work on 0x14-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,50 +1,31 @@
-#include "holberton.h"
-
+#include "main.h"
+#include <string.h>
 /**
- * _strlen - compute the length of a NULL-terminated string
- * @str: the string to measure
- *
- * Return: the length of str, or -1 if str is NULL
- */
-ssize_t _strlen(const char *str)
-{
-	ssize_t len = 0;
-
-	if (!str)
-		return (-1);
-
-	while (*str++)
-		++len;
-
-	return (len);
-}
-
-/**
- * create_file - create a file
- * @filename: the name of the file to create
- * @text_content: the data to write to filename
- *
- * Return: Upon success, return 1. Otherwise, return -1.
- */
+ * append_text_to_file - creates and wrtes
+ * @filename: file name
+ * @text_content: of letters to print
+ * Return: 1 0r -1
+*/
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t b_written = 0;
-	int fd;
+	int fd, w;
+	int len = strlen(text_content);
 
-	if (!filename)
+
+	if (filename == NULL)
 		return (-1);
-
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	if (fd < 0)
+	if (fd == -1)
 		return (-1);
 
-	if (text_content)
-		b_written = write(fd, text_content, _strlen(text_content));
+	if (text_content != NULL)
+	{
+		w = write(fd, text_content, len);
+		if (w == -1)
+			return (-1);
 
+	}
 	close(fd);
-
-	if (b_written < 0)
-		return (-1);
 	return (1);
 }
